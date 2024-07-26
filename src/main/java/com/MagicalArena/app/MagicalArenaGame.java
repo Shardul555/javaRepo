2a478ec557e240d01997cfa1cbabfe5d13855a09
223114492 <223114492@ge.com>
2024-07-26 16:47:56 +0530

    Created a POJO class: Player Information, to store name, health, strength and attack

---
+++ src/main/java/com/MagicalArena/app/MagicalArenaGame.java
@@ -0,0 +1,7 @@
+package main.java.com.MagicalArena.app;
+
+public class MagicalArenaGame {
+  public static void main(String[] args) {
+
+  }
+}
---
+++ src/main/java/com/MagicalArena/app/data/PlayerInformation.java
@@ -0,0 +1,43 @@
+package main.java.com.MagicalArena.app.data;
+
+public class PlayerInformation {
+  private String name;
+  private int health;
+  private int strength;
+  private int attack;
+
+  public PlayerInformation(String name, int health, int strength, int attack) {
+    this.name = name;
+    this.health = health;
+    this.strength = strength;
+    this.attack = attack;
+  }
+
+  public PlayerInformation() {
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+  public void setHealth(int health) {
+    this.health = health;
+  }
+  public void setStrength(int strength) {
+    this.strength = strength;
+  }
+  public void setAttack(int attack) {
+    this.attack = attack;
+  }
+  public String getName() {
+    return name;
+  }
+  public int getHealth() {
+    return health;
+  }
+  public int getStrength() {
+    return strength;
+  }
+  public int getAttack() {
+    return attack;
+  }
+}
