06ec4fa1bc39dca0c5a5d2fd5911bb8c99b32cea
223114492 <223114492@ge.com>
2024-07-28 19:44:27 +0530

    Added unit tests for service directory

--- src/test/java/com/MagicalArena/app/service/MagicalArenaControllerTest.java
+++ src/test/java/com/MagicalArena/app/service/MagicalArenaControllerTest.java
@@ -3,16 +3,18 @@ package com.MagicalArena.app.service;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.when;
+
+import java.util.Random;
 
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 
 public class MagicalArenaControllerTest {
   @Test
   void processGameInputsforUnequalHealth() {
-    MagicalArenaController magicalArenaController = Mockito.spy(new MagicalArenaController());
+    MagicalArenaController magicalArenaController = spy(new MagicalArenaController());
     magicalArenaController.processInputService = mock(ProcessInputService.class);
-    magicalArenaController.magicalArenaService = mock(MagicalArenaService.class);
     magicalArenaController.playerInformationList.get(0).setName("player1");
     magicalArenaController.playerInformationList.get(1).setName("player2");
     magicalArenaController.playerInformationList.get(0).setHealth(100);
@@ -21,17 +23,17 @@ public class MagicalArenaControllerTest {
     magicalArenaController.playerInformationList.get(1).setStrength(10);
     magicalArenaController.playerInformationList.get(0).setAttack(10);
     magicalArenaController.playerInformationList.get(1).setAttack(10);
+    Random random = mock(Random.class);
+    when(random.nextInt(1, 7)).thenReturn(1);
     doNothing().when(magicalArenaController.processInputService).processInputString(magicalArenaController.playerInformationList);
     doNothing().when(magicalArenaController.processInputService).processInputIntegers(magicalArenaController.playerInformationList);
-    magicalArenaController.processGameInputs();
     assertEquals(magicalArenaController.processGameInputs(), "player1");
   }
 
   @Test
-  void processGameInputsforEqualHealth() {
-    MagicalArenaController magicalArenaController = Mockito.spy(new MagicalArenaController());
+  void processGameInputsRandomIndexas0() {
+    MagicalArenaController magicalArenaController = spy(new MagicalArenaController());
     magicalArenaController.processInputService = mock(ProcessInputService.class);
-    magicalArenaController.magicalArenaService = mock(MagicalArenaService.class);
     magicalArenaController.playerInformationList.get(0).setName("player1");
     magicalArenaController.playerInformationList.get(1).setName("player2");
     magicalArenaController.playerInformationList.get(0).setHealth(100);
@@ -40,12 +42,34 @@ public class MagicalArenaControllerTest {
     magicalArenaController.playerInformationList.get(1).setStrength(100);
     magicalArenaController.playerInformationList.get(0).setAttack(10);
     magicalArenaController.playerInformationList.get(1).setAttack(110);
+    Random random = mock(Random.class);
+    when(random.nextInt(1, 7)).thenReturn(1);
+    when(random.nextInt(2)).thenReturn(0);
     doNothing().when(magicalArenaController.processInputService).processInputString(magicalArenaController.playerInformationList);
     doNothing().when(magicalArenaController.processInputService).processInputIntegers(magicalArenaController.playerInformationList);
     magicalArenaController.processGameInputs();
     assertEquals(magicalArenaController.processGameInputs(), "player2");
   }
 
-
+  @Test
+  void processGameInputsforRandomIndex1() {
+    MagicalArenaController magicalArenaController = spy(new MagicalArenaController());
+    magicalArenaController.processInputService = mock(ProcessInputService.class);
+    magicalArenaController.playerInformationList.get(0).setName("player1");
+    magicalArenaController.playerInformationList.get(1).setName("player2");
+    magicalArenaController.playerInformationList.get(0).setHealth(100);
+    magicalArenaController.playerInformationList.get(1).setHealth(100);
+    magicalArenaController.playerInformationList.get(0).setStrength(11);
+    magicalArenaController.playerInformationList.get(1).setStrength(100);
+    magicalArenaController.playerInformationList.get(0).setAttack(10);
+    magicalArenaController.playerInformationList.get(1).setAttack(110);
+    Random random = mock(Random.class);
+    when(random.nextInt(1, 7)).thenReturn(1);
+    when(random.nextInt(2)).thenReturn(1);
+    doNothing().when(magicalArenaController.processInputService).processInputString(magicalArenaController.playerInformationList);
+    doNothing().when(magicalArenaController.processInputService).processInputIntegers(magicalArenaController.playerInformationList);
+    magicalArenaController.processGameInputs();
+    assertEquals(magicalArenaController.processGameInputs(), "player2");
+  }
 
 }
--- src/test/java/com/MagicalArena/app/service/ProcessInputServiceTest.java
+++ src/test/java/com/MagicalArena/app/service/ProcessInputServiceTest.java
@@ -1,15 +1,68 @@
 package com.MagicalArena.app.service;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.spy;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import com.MagicalArena.app.data.PlayerInformation;
 
 
 public class ProcessInputServiceTest {
-  @Test
-  void processInputString() {
+  private ProcessInputService processInputService;
+  private List<PlayerInformation> playerInformationList;
 
+  @BeforeEach
+  public void setUp() {
+    processInputService = spy(new ProcessInputService());
+    playerInformationList = new ArrayList<>();
+    playerInformationList.add(new PlayerInformation());
+    playerInformationList.add(new PlayerInformation());
+  }
+
+  @Test
+  public void testInputString() {
+    String input = "Player1\nPlayer2";
+    InputStream in = new ByteArrayInputStream(input.getBytes());
+    System.setIn(in);
+    processInputService.processInputString(playerInformationList);
+    assertEquals("Player1", playerInformationList.get(0).getName());
+    assertEquals("Player2", playerInformationList.get(1).getName());
+  }
+
+  @Test
+  public void testInputStringWithEmptyPrefix() {
+    String input = "\n\nPlayer1\nPlayer2";
+    InputStream in = new ByteArrayInputStream(input.getBytes());
+    System.setIn(in);
+
+    processInputService.processInputString(playerInformationList);
+
+    assertEquals("Player1", playerInformationList.get(0).getName());
+    assertEquals("Player2", playerInformationList.get(1).getName());
   }
 
   @Test
   void processInputIntegers() {
+    String input = "10\n100\n1\n10\n10\n10\n";
+    InputStream in = new ByteArrayInputStream(input.getBytes());
+    System.setIn(in);
+
+    processInputService.processInputIntegers(playerInformationList);
+
+    assertEquals(10, playerInformationList.get(0).getHealth());
+    assertEquals(100, playerInformationList.get(1).getHealth());
+    assertEquals(1, playerInformationList.get(0).getStrength());
+    assertEquals(10, playerInformationList.get(1).getStrength());
+    assertEquals(10, playerInformationList.get(0).getAttack());
+    assertEquals(10, playerInformationList.get(1).getAttack());
   }
 }
